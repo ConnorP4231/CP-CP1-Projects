1beb8dd0a049da6bfa017a097b32793ef437a031
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-09-08 15:42:33 -0600

    I added a new folder and a new script in that new folder

---
+++ whoAreYou/main.py
@@ -0,0 +1,5 @@
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+color = input("What is your favorite color?: ")
+
+print("Hello", name, "you are", age, "years old and your favorite color is", color)
