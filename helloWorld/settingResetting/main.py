39b5d83b831fcd3eef44b6eac5b34deaa4cbf8ca
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-09-11 10:49:00 -0600

    I added a folder called settingResetting and I put a main.py file in that folder.

---
+++ helloWorld/settingResetting/main.py
@@ -0,0 +1,11 @@
+# Connor Pavicic, settingResetting
+
+Fac_Staff = 32
+Fac_Staff = Fac_Staff - 3
+Students = 100
+Students = Students - 1
+Attendees = Students *2
+Attendees = Attendees - 15*2
+Board = 1
+
+print((Fac_Staff+Students+Board+Attendees)/12)
