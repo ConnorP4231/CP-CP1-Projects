ad37991d1cd1b8dcab61bea25f28a032e709057c
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-09-16 10:42:34 -0600

    I changed something in the main.py script in my CtoF folder.

--- helloWorld/settingResetting/CtoF/main.py
+++ helloWorld/settingResetting/CtoF/main.py
@@ -1,4 +1,4 @@
 Celsius = int(input("What number do you want to convert?: "))
 Equation = (Celsius * 9/5)+32
 
-print("Your number is", Equation, "degrees fahrenheit.")
+print("Your number is", Equation, "degrees fahrenheit or", Celsius, "degrees celsius.")
