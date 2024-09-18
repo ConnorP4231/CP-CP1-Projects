f0d5290643e52db828216f170a9343eea91915e9
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-09-18 10:35:40 -0600

    I added a folder and a main.py script in that folder.

---
+++ averageGrade/main.py
@@ -0,0 +1,11 @@
+# Connor Pavicic, averageGrade
+
+first_class = float(input("What is the number grade of your first class? (Write .00 after your grade if it is a whole number): "))
+second_class = float(input("What is the number grade of your second class? (Write .00 after your grade if it is a whole number): "))
+third_class = float(input("What is the number grade of your third class? (Write .00 after your grade if it is a whole number): "))
+fourth_class = float(input("What is the number grade of your fourth class? (Write .00 after your grade if it is a whole number): "))
+fifth_class = float(input("What is the number grade of your fifth class? (Write .00 after your grade if it is a whole number): "))
+sixth_class = float(input("What is the number grade of your sixth class? (Write .00 after your grade if it is a whole number): "))
+seventh_class = float(input("What is the number grade of your seventh class? (Write .00 after your grade if it is a whole number): "))
+
+equation = print(float(first_class+second_class+third_class+fourth_class+fifth_class+sixth_class+seventh_class)/7)
