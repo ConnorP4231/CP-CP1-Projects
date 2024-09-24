a8b269361754247f61dfde9577309400bb66406b
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-09-24 10:49:58 -0600

    I added a folder called Madlib and a main.py file in that folder.

---
+++ Madlib/main.py
@@ -0,0 +1,13 @@
+# Connor Pavicic, Madlib
+
+noun = input("Give me a noun: ")
+noun2 = input("Give me another noun: ")
+place = input("Give me a place: ")
+adjective = input("Give me an adjective: ")
+verb = input("Give me a present-tense verb: ")
+verb2 = input("Give me another verb: ")
+num = input("Give me a number: ")
+adverb = input("Give me an adverb: ")
+exclamation = input("Give me an exclamation or a silly word: ")
+
+print("The",noun,"is in",place,". They are currently",verb,"very",adverb,". They just",verb2,num,noun2,". Anyways, that is the end of the story,",exclamation,"." )
