a7467c40ca4d2f68fc5bc4a9041ef16369d50d21
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-09-24 10:05:46 -0600

    I didn't change anything I just have to push.

--- Palandrom/main.py
+++ Palandrom/main.py
@@ -2,8 +2,9 @@
 
 word = input("Type is word to see if it is a palandrom: ")
 word_length = len(word)
+negative_length = -1*word_length
 
-if word[0:word_length]==word[0:-1*word_length]:
+if word[0:word_length]==word[0:negative_length]:
     print("this is a palandrom")
 else:
     print("this is not a palandrom")
