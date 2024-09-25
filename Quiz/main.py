222e48ac6e381a7cf10a6c0f2a361e88376a68c0
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-09-25 10:50:37 -0600

    I added a folder and a main.py file.

---
+++ Quiz/main.py
@@ -0,0 +1,15 @@
+question_1 = int(input("What is 2+2?: "))
+question_2 = int(input("What is the current year?: "))
+question_3 = input("What is the opposite of No?: ")
+question_4 = input("Will you get this question correct?: ")
+question_5 = input("What is the opposite of false?: ")
+score = 0
+
+if question_1 == 4:
+    print("correct")
+    score = score+1
+else:
+    print("incorrect")
+
+if question_2 == 2024:
+    
