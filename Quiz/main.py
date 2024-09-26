59366a75f50195def703d7a34080277adbada8bd
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-09-26 10:16:54 -0600

    I changed some code in my python file in the madlib folder.

--- Quiz/main.py
+++ Quiz/main.py
@@ -1,15 +1,33 @@
 question_1 = int(input("What is 2+2?: "))
-question_2 = int(input("What is the current year?: "))
-question_3 = input("What is the opposite of No?: ")
-question_4 = input("Will you get this question correct?: ")
-question_5 = input("What is the opposite of false?: ")
-score = 0
+question_2 = int(input("What is 9 + 10?: "))
+question_3 = int(input("How many fingers do you have?: "))
+question_4 = int(input("What is 2 raised to the 3rd power?: "))
+question_5 = int(input("what is 5 factorial?: "))
+score = 5
 
 if question_1 == 4:
-    print("correct")
-    score = score+1
+    score = score
 else:
-    print("incorrect")
+    score = score-1
 
-if question_2 == 2024:
-    
+if question_2 == 19:
+    score = score
+else:
+    score = score-1
+
+if question_3 == 5:
+    score = score
+else:
+    score = score-1
+
+if question_4 == 27:
+    score = score
+else:
+    score = score-1
+
+if question_5 == 120:
+    score = score
+else:
+    score = score-1
+
+print("You got a",score,"/5")
