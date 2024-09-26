b1d683508d06b680ea1cecabdda6eaf445ca4a48
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-09-26 10:46:13 -0600

    I added a folder called personalInfoQuiz and added a python file in that folder with code in it.

---
+++ personalInfoQuiz/main.py
@@ -0,0 +1,6 @@
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+height = input("What is your height in meters?: ")
+fav_num = input("What is your favorite number?: ")
+
+print("Hi",name,"Your favorite number is",fav_num,"or",int(fav_num),"when casted to an integer. You are",age,"years old or",int(age),"years old when casted to an int. You are",height,"meters tall or",float(height),"meters tall when casted to a float.")
