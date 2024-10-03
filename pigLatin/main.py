3f8c828079ccc248ddfaf6e1704f713317d47ddc
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-10-03 10:50:40 -0600

    I updated some code in my pig latin folder.

--- pigLatin/main.py
+++ pigLatin/main.py
@@ -10,21 +10,25 @@ def translator():
     while num_of_words>num:
         if word[num][0].lower() in vowels:
             word[num] = word[num] + 'yay'
+            str(word[num])
             num = num+1
         else:
             global word_split
             global first_char
             first_char = word[num][0]
             word[num] = str(word[num])
-            word_split = word[num][0].split
+            word_split = str(word)[num].split(word[num][0])
             word_split = str(word_split)
             word[num] = word[num][0].replace(first_char, '')
             word[num] = word[num] + word_split + 'ay'
-            word[num] = list(word[num])
+            word[num] = word[num].split()
+            word[num] = str(word[num])
+            word[num] = word[num].replace('\\', '')
+            num = num+1
     
     global translated
     translated = word
     return translated
 
 
-print(translator)
+print(translator())
