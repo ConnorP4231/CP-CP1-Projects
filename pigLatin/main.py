fa7089289b51dc913db50ce804e7506b55c1992c
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-10-02 19:02:45 -0600

    added code in pig latin file.

--- pigLatin/main.py
+++ pigLatin/main.py
@@ -1,22 +1,22 @@
 # Connor Pavicic, pigLatin
 
-def translator():    
+def translator(word_slice):    
     word = input("What do you want to translate?: ")
     vowels = ['a', 'e', 'i', 'o', 'u']
     word = word.split()
     num_of_words = len(word)
     num = 0
-
-
+    
     while num_of_words>num:
-        if word[num][0] in vowels:
-            word[num][0] = word[-1]
+        if word[num][0].lower() in vowels:
             word[num] = word[num] + 'yay'
             num = num+1
         else:
-            word[num] = word[num] + 'ay'
-            num = num + 1
-    translated_sentence = ''.join(word)
+            word_slice = word[num][0]
+            if word_slice in word[num]:
+                word[num] = word_slice
+            word[num] += word_slice
+    translated_sentence = ' '.join(word)
     return translated_sentence
 
-print(translator)
+print(translator(word_slice="word_slice"))
