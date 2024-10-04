b099f85790ae7c3291005fb347431a925eda6671
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-10-04 10:51:00 -0600

    I made yet another change to my pig latin folder and the python file in it.

--- pigLatin/main.py
+++ pigLatin/main.py
@@ -2,8 +2,21 @@
 
 def translator():    
     word = input("What do you want to translate?: ")
-    word = word[1:] + word[0] + 'ay'
-    print(word)
+    vowels = 'aeiou'
+    num = 0
+    length_word = len(word)-1
+   
+    if vowels in word[0]:
+        word = word + 'ay'
+        print(word)
+    else:
+       while length_word>num:
+            while vowels not in word[num]:
+                word = word[num+1:] + word[num]
+                num = num+1
+            else:
+                word = word + 'ay'
+                print(word)
 
 
 translator()
