4e32cf538442db06ce316cc174bcec65315f38d0
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-10-09 10:51:10 -0600

    I changed some code in the secretCypher file.

--- secretCypher/main.py
+++ secretCypher/main.py
@@ -1,3 +1,31 @@
 # Connor Pavicic, secretCypher
 
-coded_alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+def cypher():
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    secret_alphabet = 'zyxwvutsrqponmlkjihgedcba'
+    i = 0
+
+    which_decode = input("Would you like to translate or de-translate?: ")
+    if which_decode == 'translate':
+        translate = input("What would you like to translate?: ")
+        translate = list(translate)
+        length_translate = len(translate)-1
+        while i < length_translate:
+            translate[i] = secret_alphabet[i]
+            i += 1
+            final_translate = str(translate)
+        
+        return final_translate
+    elif which_decode == 'de-translate' or 'detranslate':
+        detranslate = input("What would you like to detranslate?:")
+        detranslate = str(detranslate)
+        length_de = len(detranslate)-1
+        while i < length_de:
+            detranslate[i] = alphabet[i]
+            i += 1
+            final_detranslate = str(detranslate)
+        
+        return final_detranslate
+    
+
+print(cypher())
