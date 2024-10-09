8a6ed0f0de10960e79ed4a9ff41d107424129c71
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-10-08 19:26:52 -0600

    I changed some code in my pig latin translator

--- pigLatin/main.py
+++ pigLatin/main.py
@@ -4,19 +4,18 @@ def translator():
     word = input("What word would you like to translate?: ")
     num = 0
     new_word = word
-    vowels = 'aeiou'
-    while word[num] not in vowels:
-        consonant = word[num]
-        new_word = new_word[(num+1):] + consonant
-        num = num+1
-        if word[num] not in vowels:
-            vowel = word[num]
-            new_word = vowel + new_word
-    else:
-        final_word = new_word + 'ay'
+    vowels = 'aeiouAEIOU'
+    consonants = ''
     
-    final_word = new_word + 'ay'
-    return final_word
+    while num < len(word) and word[num] not in vowels:
+        consonants += word[num]
+        num += 1
 
+    if num < len(word):
+        new_word = word[num:] + consonants + 'ay'
+    else:
+        new_word = word + 'ay'
+    
+    return new_word
 
 print(translator())
