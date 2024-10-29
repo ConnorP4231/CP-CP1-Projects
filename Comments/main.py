a0508604be87102a40c08bf551d77ff0cccabcaa
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-10-29 10:50:58 -0600

    I added a comments file and put some code in it.

---
+++ Comments/main.py
@@ -0,0 +1,24 @@
+# Connor Pavicic, Comments
+
+# name of program: Greeter
+# purpose: To greet people
+# By: Connor Pavicic
+# Created: 
+
+
+
+
+# name = input("What is your name?: ")
+
+# print(f"Hello {name}")
+
+def is_between(num):
+
+     if num > 19 and num < 21:
+
+          return True
+
+     return False
+
+num = int(input("type a number: "))
+print(is_between(num))
