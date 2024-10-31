e1a4a06046ada930bfbab7e7b41d7d901abe7e49
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-10-31 10:19:01 -0600

    i added some stuff in comments

--- Comments/main.py
+++ Comments/main.py
@@ -3,11 +3,11 @@
 # name of program: Greeter
 # purpose: To greet people
 # By: Connor Pavicic
-# Created: 
+# Created: 10/31/24
 
 
 
 
-# name = input("What is your name?: ")
+name = input("What is your name?: ") #asks the user to input their name
 
-# print(f"Hello {name}")
+print(f"Hello {name}") #prints the user's name
--- debugger/main.py
+++ debugger/main.py
@@ -1,14 +1,9 @@
 def add_to_list(item, my_list = []):
 
      my_list.append(item)
-
      return my_list
 
-
-item = input("What item do you want to add?: ")
-item_2 = input("second item: ")
-item_3 = input("third: ")
-
-print(add_to_list(item))
-print(add_to_list(item_2))
-print(add_to_list(item_3))
+     
+while True:
+     item = input("item: ")
+     print(add_to_list(item))
