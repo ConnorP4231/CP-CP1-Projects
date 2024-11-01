e4671f862a4b448a0379d302f5ea5ddd842a267f
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-10-31 22:30:46 -0600

    I added a new folder and a new file.

---
+++ listOfClases/main.py
@@ -0,0 +1,6 @@
+# Connor Pavicic, listOfClasses
+
+classes = ["Math", "Computer Science", "Chemistry", "Lunch", "Advisory", "Geography", "English", "Study Hall"]
+
+for classs in classes:
+    print(classs)
