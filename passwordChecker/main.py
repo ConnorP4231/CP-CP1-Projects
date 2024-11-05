8bf34a00835bf8eb9024eec19bc284c4887bc800
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-11-05 10:50:45 -0700

    I added a new folder and new code.

---
+++ passwordChecker/main.py
@@ -0,0 +1,6 @@
+# Connor Pavicic, passwordChecker
+
+password = input("Enter your password that is 8 characters long, includes 1 number, and includes 1 special symbol")
+
+special_symbols = ['!', '@', '#', '$', '%', '&']
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
