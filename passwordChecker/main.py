62d8667e7fa7052ac0cf5a879874ef943275d9fc
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-11-07 10:51:08 -0700

    code yes

--- passwordChecker/main.py
+++ passwordChecker/main.py
@@ -1,16 +1,21 @@
 # Connor Pavicic, passwordChecker
 
 num = 0
-special_characters = '!@#$%&*'
-numbers = '1234567890'
+special_characters = ['!', '@', '#', '$', '%', '^', '&', '*']
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-while num < 2:
-    password = input("Enter a password with atleast 8 characters, atleast 1 special character, and atleast 1 number: ")
+while num < 3:
+    password = list(input("Enter a password with atleast 8 characters, atleast 1 special character, and atleast 1 number: "))
     num = 0
     length = len(password)
 
     if special_characters in password:
         num += 1
     if numbers in password:
+        num+=1
+    if length <= 8:
         num += 1
-    if length
+    if num < 3:
+        print("Password invalid, try again.")
+else:
+    print("Password is valid.")
