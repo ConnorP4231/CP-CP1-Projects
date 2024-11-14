ab2468b9057b64b69b29947ca10548c8375efdf8
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-11-14 10:51:03 -0700

    char counter

---
+++ countingCharacters/main.py
@@ -0,0 +1,62 @@
+# Connor Pavicic, countingCharacters
+
+list1 = list(input('Type a list: ').lower())
+list2 = list(input('Type another list: ').lower())
+list3 = list(input('Type another list: ').lower())
+list4 = list(input('Type another list: ').lower())
+list5 = list(input('Type another list: ').lower())
+
+grid = [list1, list2, list3, list4, list5]
+print(grid)
+
+A = 0
+B = 0
+C = 0
+D = 0
+E = 0
+F = 0
+G = 0
+H = 0
+I = 0
+J = 0
+K = 0
+L = 0
+M = 0
+N = 0
+O = 0
+P = 0
+Q = 0
+R = 0
+S = 0
+T = 0
+U = 0
+V = 0
+W = 0
+X = 0
+Y = 0
+Z = 0
+
+for 'a' in grid:
+    A += 1
+for 'b' in grid:
+    B += 1
+for 'c' in grid:
+    C += 1
+for 'd' in grid:
+    D += 1
+for 'e' in grid:
+    E += 1
+for 'f' in grid:
+    F += 1
+for 'g' in grid:
+    G += 1
+for 'h' in grid:
+    H += 1
+for 'i' in grid:
+    I += 1
+for 'j' in grid:
+    J += 1
+for 'k' in grid:
+    K += 1
+for 'l' in grid:
+    L += 1
