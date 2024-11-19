94fd8d34421e75aadca4056d073b2b4887d7cbf7
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-11-19 10:33:48 -0700

    I added a new file and new code.

---
+++ Histogram/main.py
@@ -0,0 +1,12 @@
+# Connor Pavicic, Histogram
+
+amount = int(input('How many numbers do you want to do: '))
+count = 0
+
+while count<amount:
+    num = int(input('Enter a number: '))
+    output = ''
+    for x in range(num):
+        output += '*'
+    print(output)
+    count += 1
--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -1,6 +1,26 @@
 # Connor Pavicic, TicTacToe
 
-for top in range(1):
-    for middle in range(1):
-        for bottom in range(1):
-            print(top, middle, bottom)
+import random
+
+while True:
+    move_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    count = 1
+
+    move = int(input('Where do you want to go? (1,2,3,4,5,6,7,8,9): '))
+    move_list.remove(move)
+
+    for rows in range(3):
+        for columns in range(3):
+            print(count, end=' ')
+        print()
+    print()
+    
+    if move == 1:
+        for rows in range(3):
+            for columns in range(3):
+                if move == 1:
+                    continue
+                print('O')
+            else:
+                print(count, end=' ')
