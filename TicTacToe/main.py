a47b51c41386322481a681e7a9b91174309aaac8
ConnorP4231 <connor.pavicic@ucas-edu.net>
2024-11-19 21:39:08 -0700

    I might have figured it out

--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -2,20 +2,20 @@
 
 import random
 
-count = 1
-num = input('enter num: ')
+#1 = 0 0
+#2 = 0 1
+#3 = 0 2
+#4 = 1 0
+#5 = 1 1
+#6 = 1 2
+#7 = 2 0
+#8 = 2 1
+#9 = 2 2
 
-for rows in range(3):
-    for columns in range(3):
-        print(count, end=' ')
-        count += 1
-    print()
-
-for rows in range(3):
-    for columns in range(3):
-        for num in rows or num in columns:
-            print('X')
+for row in range(3):
+    for column in range(3):
+        if row == 1 and column == 0:
+            print('-', end=' ')
         else:
-            print(count, end='')
-            count += 1
+            print('*', end=' ')
     print()
